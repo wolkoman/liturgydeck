5a46044f13cbdeeaecdb0bfdcd4828cf2f4696b8
wolkoman <superwolko@gmail.com>
2020-09-03 12:31:50 +0200

    Update styling

--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,206 @@
-import OBSWebSocket from 'obs-websocket-js';
-import { StreamDeck } from './streamDeck';
-import textgenFile from './textgen';
-import imageFile from './image';
-import { WebServer } from './webserver';
+import OBSWebSocket from "obs-websocket-js";
+import { StreamDeck } from "./streamDeck";
+import textgenFile, { TextgenType } from "./textgen";
+import imageFile from "./image";
+import { WebServer } from "./webserver";
 
-const { openStreamDeck } = require('elgato-stream-deck')
-const fetch = require('node-fetch');
+const { openStreamDeck } = require("elgato-stream-deck");
+const fetch = require("node-fetch");
 const obs = new OBSWebSocket();
 let myStreamDeck: StreamDeck;
 let webServer = new WebServer(80);
 
-try{
-    myStreamDeck = openStreamDeck();
-}catch(e){
-    console.log("Error connecting to StreamDeck");
-    process.exit();
+const CAMERA_SCENE_NAME = "KAMERA";
+const AUDIO_SCENE_NAME = "TON";
+
+try {
+  myStreamDeck = openStreamDeck();
+} catch (e) {
+  console.log("Error connecting to StreamDeck");
+  process.exit();
 }
 
 const textgen = textgenFile(myStreamDeck);
 const image = imageFile(myStreamDeck);
 
 const fields = {
-    scenes: [0,1,2,5,6,7,10,11],
-    camera: [8,9],
-    up: 3,
-    down: 4,
-    nextPage: 12,
+  scenes: [0, 1, 2, 5, 6, 7, 10, 11],
+  camera: [8, 9],
+  up: 3,
+  down: 4,
+  nextPage: 12,
 };
 
 myStreamDeck.clearAllKeys();
 const fieldsDown = Array(myStreamDeck.NUM_KEYS).fill(0);
-let scenes: any[] = [], activeCameraIndex = 0, warnCameraIndex = 0, cameraScene: any, activeScene: any, currentPage = 0;
+let scenes: any[] = [],
+  activeCameraIndex = 0,
+  warnCameraIndex = 0,
+  cameraScene: any,
+  activeScene: any,
+  currentPage = 0;
 
 obs
-    .connect({ address: 'localhost:4444'})
-    .then(() => updateScenes())
-    .catch(() => console.log("OBS could not connect!"));
-obs.on('ScenesChanged', () => updateScenes());
-obs.on('SwitchScenes', (data: any) => setActiveScene(data));
+  .connect({ address: "localhost:4444" })
+  .then(() => updateScenes())
+  .catch(() => console.log("OBS could not connect!"));
+obs.on("ScenesChanged", () => updateScenes());
+obs.on("SwitchScenes", (data: any) => setActiveScene(data));
 
 const updateScenes = () => {
-    obs.send('GetSceneList').then((info: any) => {
-        scenes = info.scenes.map((scene: number, id: number) => ({...(scene as any), id }));
-        cameraScene = scenes.find(scene => scene.name === 'KAMERA');
-        renderCamera();
-        renderPage(currentPage);
-    });
+  obs.send("GetSceneList").then((info: any) => {
+    scenes = info.scenes.map((scene: number, id: number) => ({
+      ...(scene as any),
+      id,
+    }));
+    cameraScene = scenes.find((scene) => scene.name === CAMERA_SCENE_NAME);
+    renderCamera();
+    renderNext();
+    renderPage(currentPage);
+  });
 };
 const renderPage = (page: number) => {
-    currentPage = page;
-    const scenesPart = scenes.slice(currentPage * fields.scenes.length,(currentPage+1) * fields.scenes.length);
-    scenesPart.forEach(scene => renderScene(scene));
-    Array(fields.scenes.length - scenesPart.length).fill(0).forEach((x,i) => 
-        myStreamDeck.fillColor(fields.scenes[fields.scenes.length-1-i], 0, 0, 0)
+  currentPage = page;
+  const scenesPart = scenes.slice(
+    currentPage * fields.scenes.length,
+    (currentPage + 1) * fields.scenes.length
+  );
+  scenesPart.forEach((scene) => renderScene(scene));
+  Array(fields.scenes.length - scenesPart.length)
+    .fill(0)
+    .forEach((x, i) =>
+      myStreamDeck.fillColor(
+        fields.scenes[fields.scenes.length - 1 - i],
+        0,
+        0,
+        0
+      )
     );
-}
+};
 const renderCamera = () => {
-    if(cameraScene){
-        Array(fields.camera.length - cameraScene.sources.length).fill(0).forEach((x,i) => 
-            myStreamDeck.fillColor(fields.camera[fields.camera.length-1-i], 0, 0, 0)
-        );
-        cameraScene.sources.forEach(async (source: any, i: number) => myStreamDeck.fillImage(
-            fields.camera[i],
-            await textgen('KAMERA '+source.name, activeCameraIndex === i ? 1 : (warnCameraIndex === i ? 2 : 0))
-        ));
-    }
-}
+  if (cameraScene) {
+    Array(fields.camera.length - cameraScene.sources.length)
+      .fill(0)
+      .forEach((x, i) =>
+        myStreamDeck.fillColor(
+          fields.camera[fields.camera.length - 1 - i],
+          0,
+          0,
+          0
+        )
+      );
+    cameraScene.sources.forEach(async (source: any, i: number) =>
+      myStreamDeck.fillImage(
+        fields.camera[i],
+        await textgen(
+          "CAMERA " + source.name,
+          TextgenType.CAMERA,
+          activeCameraIndex === i,
+          warnCameraIndex === i
+        )
+      )
+    );
+  }
+};
 const renderScene = async (scene: any) => {
-    let fieldIndex = scene.id - currentPage * fields.scenes.length;
-    if(fieldIndex >= 0 && fieldIndex < fields.scenes.length){
-        myStreamDeck.fillImage(
-            fields.scenes[fieldIndex],
-            await textgen(scene.name, (activeScene && scene.id === activeScene.id) ? 1 : 0)
-        );
-    }
-}
+  let fieldIndex = scene.id - currentPage * fields.scenes.length;
+  if (fieldIndex >= 0 && fieldIndex < fields.scenes.length) {
+    myStreamDeck.fillImage(
+      fields.scenes[fieldIndex],
+      await textgen(
+        scene.name,
+        TextgenType.SCENE,
+        activeScene && scene.id === activeScene.id
+      )
+    );
+  }
+};
+const renderNext = async () => {
+  myStreamDeck.fillImage(
+    fields.nextPage,
+    await textgen("NEXT", TextgenType.NEXT)
+  );
+};
 let browser: any;
 const setActiveScene = async (info: any) => {
-    let lastActiveScene = activeScene;
-    activeScene = scenes.find(scene => scene.name === info.sceneName);
-    if(lastActiveScene) renderScene(lastActiveScene);
-    renderScene(activeScene);
+  let lastActiveScene = activeScene;
+  activeScene = scenes.find((scene) => scene.name === info.sceneName);
+  if (lastActiveScene) renderScene(lastActiveScene);
+  renderScene(activeScene);
 
-    if(activeScene){
-        browser = activeScene.sources.find((source: any) => source.type === 'browser_source');
-        if(browser){
-            myStreamDeck.fillImage(fields.up, await image('../fixtures/left.png'));
-            myStreamDeck.fillImage(fields.down, await image('../fixtures/right.png'));
-        }else{
-            myStreamDeck.fillColor(fields.up, 150, 150, 150);
-            myStreamDeck.fillColor(fields.down, 150, 150, 150);
-        }
+  if (activeScene) {
+    browser = activeScene.sources.find(
+      (source: any) => source.type === "browser_source"
+    );
+    if (browser) {
+      myStreamDeck.fillImage(fields.up, await image("../fixtures/left.png"));
+      myStreamDeck.fillImage(fields.down, await image("../fixtures/right.png"));
+    } else {
+      myStreamDeck.fillColor(fields.up, 50, 50, 50);
+      myStreamDeck.fillColor(fields.down, 50, 50, 50);
     }
-}
+  }
+};
 
-const nextPage = () => renderPage((scenes.length > (currentPage+1) * fields.scenes.length) ? (currentPage + 1) : 0);
+const nextPage = () =>
+  renderPage(
+    scenes.length > (currentPage + 1) * fields.scenes.length
+      ? currentPage + 1
+      : 0
+  );
 
-myStreamDeck.on('down', (keyIndex: number) => {
-
-    fieldsDown[keyIndex] = new Date().getTime();
-    if(fields.scenes.includes(keyIndex)){
-        let sceneIndex = currentPage * fields.scenes.length + fields.scenes.indexOf(keyIndex);
-        if(sceneIndex < scenes.length){
-            obs.send('SetCurrentScene', {'scene-name': scenes[sceneIndex].name})
-            setActiveScene({sceneName: scenes[sceneIndex].name});
-        }
-    }else if(keyIndex === fields.up && browser){
-        webServer.emitBrowserSource('ArrowUp');
-    }else if(keyIndex === fields.down && browser){
-        webServer.emitBrowserSource('ArrowDown');
-    }else if(keyIndex === fields.nextPage){
-        nextPage();
+myStreamDeck.on("down", (keyIndex: number) => {
+  fieldsDown[keyIndex] = new Date().getTime();
+  if (fields.scenes.includes(keyIndex)) {
+    let sceneIndex =
+      currentPage * fields.scenes.length + fields.scenes.indexOf(keyIndex);
+    if (sceneIndex < scenes.length) {
+      obs.send("SetCurrentScene", { "scene-name": scenes[sceneIndex].name });
+      setActiveScene({ sceneName: scenes[sceneIndex].name });
     }
+  } else if (keyIndex === fields.up && browser) {
+    webServer.emitBrowserSource("ArrowUp");
+  } else if (keyIndex === fields.down && browser) {
+    webServer.emitBrowserSource("ArrowDown");
+  } else if (keyIndex === fields.nextPage) {
+    nextPage();
+  }
 });
-myStreamDeck.on('up', (keyIndex: number) => {
-    if(fields.camera.includes(keyIndex)){
-        let time = new Date().getTime() - fieldsDown[keyIndex];
+myStreamDeck.on("up", (keyIndex: number) => {
+  if (fields.camera.includes(keyIndex)) {
+    let time = new Date().getTime() - fieldsDown[keyIndex];
 
-        if(cameraScene && cameraScene.sources.length > activeCameraIndex){
-            if(time < 200){
-                activeCameraIndex = fields.camera.findIndex(i => i === keyIndex);
-                warnCameraIndex = -1;
-                cameraScene.sources
-                    .filter((source: any, i: number) => i !== activeCameraIndex)
-                    //@ts-ignore
-                    .forEach((source: any) => obs.send('SetSceneItemProperties', {item: source.name as string,"scene-name":"KAMERA", visible: false}).catch(console.log));
-                //@ts-ignore
-                obs.send('SetSceneItemProperties', {item: cameraScene.sources[activeCameraIndex].name as string,"scene-name":"KAMERA", visible: true});
-                webServer.setActiveCamera(activeCameraIndex);
-            }else{
-                warnCameraIndex = fields.camera.findIndex(i => i === keyIndex);
-                webServer.setWarnCamera(activeCameraIndex);
-            }
-            renderCamera();
-        }
+    if (cameraScene && cameraScene.sources.length > activeCameraIndex) {
+      if (time < 200) {
+        activeCameraIndex = fields.camera.findIndex((i) => i === keyIndex);
+        warnCameraIndex = -1;
+        cameraScene.sources
+          .filter((source: any, i: number) => i !== activeCameraIndex)
+          //@ts-ignore
+          .forEach((source: any) =>
+            obs
+              .send("SetSceneItemProperties", {
+                item: source.name as string,
+                ["scene-name"]: CAMERA_SCENE_NAME,
+                visible: false,
+              } as any)
+              .catch(console.log)
+          );
+        //@ts-ignore
+        obs.send("SetSceneItemProperties", {
+          item: cameraScene.sources[activeCameraIndex].name as string,
+          "scene-name": CAMERA_SCENE_NAME,
+          visible: true,
+        });
+        webServer.setActiveCamera(activeCameraIndex);
+      } else {
+        warnCameraIndex = fields.camera.findIndex((i) => i === keyIndex);
+        webServer.setWarnCamera(activeCameraIndex);
+      }
+      renderCamera();
     }
+  }
 });
-myStreamDeck.on('error', (error: string) => {
-	console.error(error)
+myStreamDeck.on("error", (error: string) => {
+  console.error(error);
 });
-
--- src/textgen.ts
+++ src/textgen.ts
@@ -1,68 +1,106 @@
-const sharp = require('sharp')
-const path = require('path')
-const PImage = require('pureimage')
-const streamBuffers = require('stream-buffers')
+import { SSL_OP_SSLREF2_REUSE_CERT_TYPE_BUG } from "constants";
 
-PImage.registerFont(path.resolve(__dirname, '../fixtures/SourceSansPro-Regular.ttf'), 'Source Sans Pro').load();
+const sharp = require("sharp");
+const path = require("path");
+const PImage = require("pureimage");
+const streamBuffers = require("stream-buffers");
 
+PImage.registerFont(
+  path.resolve(__dirname, "../fixtures/SourceSansPro-Regular.ttf"),
+  "Source Sans Pro"
+).load();
 
-function wrapText(context: any, text: string, x: number, y: number, maxWidth: number, lineHeight: number) {
-    var words = text.split(' ');
-    var line = '';
+function wrapText(
+  context: any,
+  text: string,
+  x: number,
+  y: number,
+  maxWidth: number,
+  lineHeight: number,
+  color: string
+) {
+  var words = text.split(" ");
+  var line = "";
+  context.font = `${lineHeight}pt "Source Sans Pro"`;
+  context.fillStyle = color;
 
-    for(var n = 0; n < words.length; n++) {
-      var testLine = line + words[n] + ' ';
-      var metrics = context.measureText(testLine);
-      var testWidth = metrics.width;
-      if (testWidth > maxWidth && n > 0) {
-        context.fillText(line, x, y);
-        line = words[n] + ' ';
-        y += lineHeight;
-      }
-      else {
-        line = testLine;
-      }
+  for (var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + " ";
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + " ";
+      y += lineHeight;
+    } else {
+      line = testLine;
     }
-    context.textAlign = "center";
-    context.fillText(line, x, y);
+  }
+  context.textAlign = "center";
+  context.font;
+  context.fillText(line, x, y);
+}
+
+export enum TextgenType {
+  SCENE,
+  CAMERA,
+  NEXT,
+}
+export default (streamDeck: any) => async (
+  textString: string,
+  type: TextgenType,
+  active = false,
+  warn = false
+) => {
+  const img = PImage.make(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE);
+  const ctx = img.getContext("2d");
+  ctx.USE_FONT_GLYPH_CACHING = false;
+
+  if (type === TextgenType.SCENE) {
+    ctx.fillStyle = active ? "#ffffff" : "#000000";
+    ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE);
+    wrapText(ctx, "SCENE", 2, 16, 60, 15, active ? "#000000" : "#aaaaaa");
+    wrapText(ctx, textString, 2, 30, 60, 16, active ? "#000000" : "#ffffff");
+  } else if (type === TextgenType.CAMERA) {
+    ctx.fillStyle = active ? "red" : warn ? "#ffff00" : "black";
+    ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE);
+    const border = 6;
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(
+      border,
+      border,
+      streamDeck.ICON_SIZE - border * 2,
+      streamDeck.ICON_SIZE - border * 2 - 2
+    );
+    ctx.globalAlpha = 1;
+    wrapText(ctx, textString, 6, 18, 60, 16, "#ffffff");
+  } else if (type === TextgenType.NEXT) {
+    wrapText(ctx, textString, 2, 16, 60, 15, "#aaaaaa");
   }
 
-export default (streamDeck: any) => async (textString: string, style = 0) => {
+  const writableStreamBuffer = new streamBuffers.WritableStreamBuffer({
+    initialSize: 20736, // Start at what should be the exact size we need
+    incrementAmount: 1024, // Grow by 1 kilobyte each time buffer overflows.
+  });
 
-    const img = PImage.make(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
-    const ctx = img.getContext('2d');
-    ctx.fillStyle = ['#000022', '#223377', '#888800'][style];
-    ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE) // As of v0.1, pureimage fills the canvas with black by default.
-    ctx.font = '16pt "Source Sans Pro"'
-    ctx.USE_FONT_GLYPH_CACHING = false
-    ctx.fillStyle = ['#ffffff', '#ffffff', '#ffffff'][style];
-    wrapText(ctx, textString, 2, 18, 60, 16);
+  try {
+    await PImage.encodePNGToStream(img, writableStreamBuffer);
 
-    const writableStreamBuffer = new streamBuffers.WritableStreamBuffer({
-        initialSize: 20736, // Start at what should be the exact size we need
-        incrementAmount: 1024 // Grow by 1 kilobyte each time buffer overflows.
+    const pngBuffer = await sharp({
+      create: {
+        width: streamDeck.ICON_SIZE,
+        height: streamDeck.ICON_SIZE,
+        channels: 4,
+        background: { r: 0, g: 0, b: 0, alpha: 1 },
+      },
     })
-
-    try {
-        await PImage.encodePNGToStream(img, writableStreamBuffer)
-
-        const pngBuffer = await sharp({
-          create: {
-          width: streamDeck.ICON_SIZE,
-          height: streamDeck.ICON_SIZE,
-          channels: 4,
-          background: { r: 0, g: 0, b: 0, alpha: 1 }
-          }
-        })
-          .composite([{input: writableStreamBuffer.getContents()}])
-          .png()
-          .toBuffer()
-        const finalBuffer = await sharp(pngBuffer)
-          .flatten()
-          .raw()
-          .toBuffer()
-        return (finalBuffer);
-    } catch (error) {
-        console.error(error)
-    }
+      .composite([{ input: writableStreamBuffer.getContents() }])
+      .png()
+      .toBuffer();
+    const finalBuffer = await sharp(pngBuffer).flatten().raw().toBuffer();
+    return finalBuffer;
+  } catch (error) {
+    console.error(error);
+  }
 };
